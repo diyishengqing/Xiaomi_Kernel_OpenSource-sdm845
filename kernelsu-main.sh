30cfcdf183cd92f41c8535d74951097dac2eaedb
diyishengqing <146532963+diyishengqing@users.noreply.github.com>
2023-11-13 02:31:15 +0800

    Update kernelsu-main.sh

--- kernelsu-main.sh
+++ kernelsu-main.sh
@@ -1,2 +1,2 @@
 #kernelsu-main.sh
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.7.0
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.6.9
